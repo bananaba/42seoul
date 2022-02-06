7de969d900fd6055d7f525c51dc55e4113cf0e96
이백범 <balee@ibaegbeom-ui-MacBookPro.local>
2022-02-06 18:29:22 +0900

    M

--- 42cursus/Libft/ft_strjoin.c
+++ 42cursus/Libft/ft_strjoin.c
@@ -20,6 +20,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 && !s2)
 		return (0);
+	else if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
 	temp = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!temp)
 		return (0);
