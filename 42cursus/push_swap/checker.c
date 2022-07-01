9573dde978edbcef6cf7573961f887705d8c6078
이백범 <balee@ibaegbeom-ui-MacBookPro.local>
2022-07-01 18:46:13 +0900

    M

--- 42cursus/push_swap/checker.c
+++ 42cursus/push_swap/checker.c
@@ -6,7 +6,7 @@
 /*   By: balee <balee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:05:45 by balee             #+#    #+#             */
-/*   Updated: 2022/07/01 18:31:37 by balee            ###   ########.fr       */
+/*   Updated: 2022/07/01 18:39:57 by balee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,11 +116,11 @@ int	main(int argc, char *argv[])
 		if (status)
 		{
 			write(1, "Error\n", 6);
-			free_all(&info);
+			break ;
 		}
 	}
 	free_all_t_tree(root);
-	if (checking(&info))
+	if (!status && checking(&info))
 		write(1, "KO\n", 3);
 	free_all(&info);
 	return (0);
--- 42cursus/push_swap/push_swap.c
+++ 42cursus/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: balee <balee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 05:18:36 by balee             #+#    #+#             */
-/*   Updated: 2022/07/01 18:27:23 by balee            ###   ########.fr       */
+/*   Updated: 2022/07/01 18:39:46 by balee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,11 @@ int	main(int argc, char *argv[])
 		if (status)
 		{
 			write(1, "Error\n", 6);
-			free_all(&info);
+			break ;
 		}
 	}
-	sort(&info, info.a_factor, 0, root);
+	if (!status)
+		sort(&info, info.a_factor, 0, root);
 	free_all_t_tree(root);
 	free_all(&info);
 	return (0);
