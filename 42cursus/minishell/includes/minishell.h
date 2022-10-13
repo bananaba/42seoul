842537032fbb3e38deaf1d83795e8b058d490b73
이백범 <balee@ibaegbeom-ui-MacBookPro.local>
2022-10-13 16:48:44 +0900

    M

---
+++ 42cursus/minishell/includes/minishell.h
@@ -0,0 +1,11 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include	<stdlib.h>
+# include	<unistd.h>
+# include	<stdio.h>
+# include	<errno.h>
+# include	<signal.h>
+# include	<fcntl.h>
+
+#endif
---
+++ 42cursus/minishell/srcs/main.c
@@ -0,0 +1,9 @@
+#include "../includes/minishell.h"
+
+void	set_signal(void)
+{
+}
+
+int	main(void)
+{
+}
