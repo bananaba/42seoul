091df0435532ff521f5163e54e51d531f33c3bb4
Baekbeom Lee <balee@c7r2s1.42seoul.kr>
2022-10-19 22:22:03 +0900

    M

--- 42cursus/minishell/test.c
+++ 42cursus/minishell/test.c
@@ -16,25 +16,25 @@ void	process(char *envp[])
 	char	*cmd[3];
 	char	*c;
 
-//	cmd[0] = strdup("/Users/balee/42/42cursus/minishell/echo");
-	cmd[0] = strdup("/bin/echo");
-	cmd[1] = strdup("A = $A");
+	cmd[0] = strdup("/Users/balee/42/42cursus/minishell/echo");
+//	cmd[0] = strdup("/bin/echo");
+	cmd[1] = strdup("a*");
 	cmd[2] = NULL;
 	execve(cmd[0], cmd, envp);
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
-//	pid_t	pid;
+	pid_t	pid;
 
 //	for (int i = 0; envp[i]; i++)
 //		printf("%s\n", envp[i]);
 //	printf("A = $A\n");
-//	pid = fork();
-//	if (pid == 0)
-//		process(envp);
-//	else
-//		wait(NULL);
-	printf("%s", 12311231);
+	pid = fork();
+	if (pid == 0)
+		process(envp);
+	else
+		wait(NULL);
+//	printf("%s", 12311231);
 	return (0);
 }
