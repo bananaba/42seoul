d4a9aa900b5991d3ed02d60550c2342da4bac36e
Baekbeom Lee <balee@c2r5s4.42seoul.kr>
2022-11-20 06:18:16 +0900

    M

--- 42cursus/cpp_module/cpp01/ex01/PhoneBook.cpp
+++ 42cursus/cpp_module/cpp01/ex01/PhoneBook.cpp
@@ -2,20 +2,24 @@
 
 int	main(void)
 {
-	PhoneBook	info[8];
+	PhoneBook	book;
 	std::string	cmd;
 	int			i;
 
+	i = 0;
 	while (true)
 	{
-		std::cout << ">";
-		std::getline(std::cin, cmd);
-		if (cmd == "ADD")
-			;
-		else if (cmd == "SEARCH")
-			;
-		else if (cmd == "EXIT")
-			;
+		std::cout << "> ";
+		std::getline(std::cin >> std::ws, cmd);
+		if (cmd.compare("ADD") == 0)
+		{
+			book.add(i);
+			i = (i + 1) % 8;
+		}
+		else if (cmd.compare("SEARCH") == 0)
+			book.search();
+		else if (cmd.compare("EXIT") == 0)
+			break ;
 	}
 	return (0);
 }
--- 42cursus/cpp_module/cpp01/ex01/PhoneBook.hpp
+++ 42cursus/cpp_module/cpp01/ex01/PhoneBook.hpp
@@ -5,7 +5,7 @@
 # include <string>
 # include <iomanip>
 
-class PhoneBook
+class Contact
 {
 	private:
 		std::string	first_name;
@@ -14,17 +14,89 @@ class PhoneBook
 		std::string	phone_number;
 		std::string	darkest_secret;
 	public:
-		phonebook(/* args */);
-		~phonebook();
-	};
+		Contact(void)
+		{
+			first_name = "";
+			last_name = "";
+			nickname = "";
+			phone_number = "";
+			darkest_secret = "";
+		}
+		~Contact(void) {}
+		void	add(void);
+		int		print_info(int i);
+};
 
-	phonebook::phonebook(/* args */)
-	{
-	}
+class PhoneBook
+{
+	private:
+		Contact info[8];
+	public:
+		PhoneBook(void) {}
+		~PhoneBook(void) {}
+		void	add(int i);
+		void	search(void);
+};
 
-	phonebook::~phonebook()
-	{
+void	PhoneBook::add(int i)
+{
+	info[i].add();
 }
 
+void	PhoneBook::search(void)
+{
+	int	i;
+
+	i = 0;
+	std::cout << std::right << std::setw(10) << "index" << "|"
+		<< std::right << std::setw(10) << "first name" << "|"
+		<< std::right << std::setw(10) << "last name" << "|"
+		<< std::right << std::setw(10) << "nickname" << std::endl;
+	while (info[i].print_info(i))
+		i++;
+	while (true)
+	{
+		std::cout << "> ";
+		std::cin >> i;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+		}
+	}
+}
+
+void	Contact::add(void)
+{
+	std::cout << std::left << std::setw(15) << "first_name" << ": ";
+	std::getline(std::cin >> std::ws, first_name);
+	std::cout << std::left << std::setw(15) << "last_name" << ": ";
+	std::getline(std::cin >> std::ws, last_name);
+	std::cout << std::left << std::setw(15) << "nickname" << ": ";
+	std::getline(std::cin >> std::ws, nickname);
+	std::cout << std::left << std::setw(15) << "phone_number" << ": ";
+	std::getline(std::cin >> std::ws, phone_number);
+	std::cout << std::left << std::setw(15) << "secret" << ": ";
+	std::getline(std::cin >> std::ws, darkest_secret);
+}
+
+int	Contact::print_info(int i)
+{
+	if (first_name.empty())
+		return (false);
+	std::cout << std::right << std::setw(10) << i + 1  << "|";
+	if (first_name.length() <= 10)
+		std::cout << std::right << std::setw(10) << first_name << "|";
+	else
+		std::cout << first_name.substr(0, 9) << "." << "|";
+	if (last_name.length() <= 10)
+		std::cout << std::right << std::setw(10) << last_name << "|";
+	else
+		std::cout << last_name.substr(0, 9) << "." << "|";
+	if (nickname.length() <= 10)
+		std::cout << std::right << std::setw(10) << nickname << std::endl;
+	else
+		std::cout << nickname.substr(0, 9) << "." << std::endl;
+	return (true);
+}
 
 #endif
